13c6aa269614fcc7ebd232f25ce961403071c78c
Emily Houlihan <houlihan.e.j@gmail.com>
2016-11-15 23:57:29 -0500

    started history of crests vis and also tried to get parallax working

--- js/main.js
+++ js/main.js
@@ -1,51 +1,29 @@
-L.Icon.Default.imagePath = 'images/';
 
-var allData = [];
+// Function to convert date objects to strings or reverse
+var dateFormatter = d3.time.format("%m/%d/%Y");
 
-// Variable for the visualization instance
-var stationMap;
+var crestChart;
 
-var mbta;
-
-// Start application by loading the data
-loadData();
-
-
-function loadData() {
-
-	// Proxy url
-	var proxy = 'http://michaeloppermann.com/proxy.php?format=xml&url=';
-
-  // Hubway XML station feed
-    var url = 'https://www.thehubway.com/data/stations/bikeStations.xml';
-
-  // TO-DO: LOAD DATA
-    d3.json(proxy + url, function(error, data){
-        allData = data.station;
-        allData.forEach(function(d){
-            d.id = +d.id;
-            d.nbEmptyDocks = +d.nbEmptyDocks;
-            d.nbBikes = +d.nbBikes;
-            d.lat = +d.lat;
-            d.long = +d.long;
-        });
-
-        $("#station-count").html(allData.length);
-
-        d3.json("mbta.json", function(data) {
-            console.log(data);
-            mbta = data;
-
-            createVis();
-        });
+// (1) Load data asynchronously
+queue()
+    .defer(d3.csv,"data/crests.csv")
+    .defer(d3.json, "data/stages.json")
+    .await(createVis);
 
 
+function createVis(error, crestData, stageData){
+    if(error) { console.log(error); }
+
+    crestData.forEach(function(e){
+        e.date = dateFormatter.parse(e.date);
+        e.height = +e.height;
     });
-}
+
+    for(var key in stageData){
+        stageData[key] = +stageData[key]
+    }
 
 
-function createVis() {
-   // TO-DO: INSTANTIATE VISUALIZATION
-   stationMap = new StationMap("station-map", allData, [42.360082, -71.058880], mbta);
+    crestChart = new CrestChart("flood-history-chart", crestData, stageData);
 
 }
