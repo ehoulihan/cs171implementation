a657236d64f38f59788a665146d5d43e719787fc
Emily Houlihan <houlihan.e.j@gmail.com>
2016-11-28 14:45:40 -0500

    discharge chart first draft

--- js/main.js
+++ js/main.js
@@ -2,17 +2,19 @@
 
 // Function to convert date objects to strings or reverse
 var dateFormatter = d3.time.format("%m/%d/%Y");
+var dischargeDateFormatter = d3.time.format("%Y-%m-%d %H:%M");
 
-var crestChart;
+var crestChart, dischargeChart;
 
 // (1) Load data asynchronously
 queue()
     .defer(d3.csv,"data/crests.csv")
     .defer(d3.json, "data/stages.json")
+    .defer(d3.csv, "data/discharge15s.csv")
     .await(createVis);
 
 
-function createVis(error, crestData, stageData){
+function createVis(error, crestData, stageData, dischargeData){
     if(error) { console.log(error); }
 
     crestData.forEach(function(e){
@@ -24,7 +26,18 @@ function createVis(error, crestData, stageData){
         stageData[key] = +stageData[key]
     }
 
+    dischargeData.forEach(function(e){
+        e.discharge = +e.discharge;
+        e.timestamp = dischargeDateFormatter.parse(e.timestamp);
+    })
+
+    dischargeData = dischargeData.filter(function(e){
+        return e.timestamp > dateFormatter.parse("8/1/2011") && e.timestamp < dateFormatter.parse("10/20/2011");
+    });
+
 
     crestChart = new CrestChart("flood-history-chart", crestData, stageData, 'see-years');
+
+    dischargeChart = new DischargeChart("discharge-chart", dischargeData);
 
 }
