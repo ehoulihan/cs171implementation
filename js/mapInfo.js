cd408807cff9594406a775fcc1ab0666c2083c83
jmartinez14 <jhavmar@gmail.com>
2016-12-10 11:37:46 -0500

    playing with buttons

--- js/mapInfo.js
+++ js/mapInfo.js
@@ -2,12 +2,10 @@
  * Created by jackiemartinez on 12/6/16.
  */
 var mapInfo = [
-    "The Lock 7 Dam is located along the Mohawk River, and serves as both a lock system and as a hydro-electric plant",
-    "The Schenectady Stockade district has historically suffered severe flooding.",
-    "The area is residential",
-    "The FEMA flood maps show the 100-year flood areas, or areas in which there is a 1%" +
+    "1 - The Lock 7 Dam is located along the Mohawk River, and serves as both a lock system and as a hydro-electric plant",
+    "2 - The Schenectady Stockade district has historically suffered severe flooding.",
+    "3 - During Hurricane Irene, certain low-elevation areas were susceptible to flooding, including many residential and industrial areas",
+    "5 - The FEMA flood maps show the 100-year flood areas, or areas in which there is a 1%" +
     "chance annually that there will be a flood that inundates the area",
-    "Below, we focus on the actual profile of Ingersoll Road, which was flooded during Hurricane Irene",
-    "The Schenectady Stockade suffered severe amounts of damage during the floods.  In this map you can see the extent of the" +
-    "flooding area, in addition to the flooded areas, of particular interest are the Lock 7 and Lock 8 dams."
-]
+    "5 - On the next slide, we focus on the actual profile of Ingersoll Street, which was affected by Hurrican Irene"
+];
